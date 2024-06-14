21cf7d89bd1c8764052d2daf6665d36949fef18a
José Harão P. Lima <harao.lima@yahoo.com.br>
2024-06-14 00:03:12 -0300

    Criação do perfil de Dev

---
+++ src/main/java/com/inforcentersistemas/helpdesk/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.inforcentersistemas.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.inforcentersistemas.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+	@Bean
+    Boolean instanciaDB() {
+		if (value.equals("create")) {
+			this.dbService.instanciaDB();	
+			return true;
+		}
+		return false;
+	}
+}
