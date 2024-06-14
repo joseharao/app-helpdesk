47324256be596709833076c12c8395b2d12d928b
José Harão P. Lima <harao.lima@yahoo.com.br>
2024-06-14 00:30:41 -0300

    Criação do repository tecnico

---
+++ src/main/java/com/inforcentersistemas/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,24 @@
+package com.inforcentersistemas.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.inforcentersistemas.helpdesk.domains.Tecnico;
+import com.inforcentersistemas.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "api/tecnicos")
+public class TecnicoResource {
+	@Autowired
+	private TecnicoService tecnicoService;
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+		Tecnico obj = this.tecnicoService.findById(id);
+		
+		return ResponseEntity.ok(obj);
+	}
+}
