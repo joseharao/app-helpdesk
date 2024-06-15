47927b47a07b8cff96389e4201989cda1188c194
José Harão P. Lima <harao.lima@yahoo.com.br>
2024-06-14 22:43:20 -0300

    Implementado o Crud Cliente

--- src/main/java/com/inforcentersistemas/helpdesk/domains/Cliente.java
+++ src/main/java/com/inforcentersistemas/helpdesk/domains/Cliente.java
@@ -2,8 +2,10 @@ package com.inforcentersistemas.helpdesk.domains;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.inforcentersistemas.helpdesk.domains.dtos.ClienteDTO;
 import com.inforcentersistemas.helpdesk.domains.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -36,5 +38,15 @@ public class Cliente extends Pessoa {
 	}
 	
 	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
 	
 }
---
+++ src/main/java/com/inforcentersistemas/helpdesk/services/ClienteService.java
@@ -0,0 +1,71 @@
+package com.inforcentersistemas.helpdesk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inforcentersistemas.helpdesk.domains.Pessoa;
+import com.inforcentersistemas.helpdesk.domains.Cliente;
+import com.inforcentersistemas.helpdesk.domains.dtos.ClienteDTO;
+import com.inforcentersistemas.helpdesk.repositories.PessoaRepository;
+import com.inforcentersistemas.helpdesk.repositories.ClienteRepository;
+import com.inforcentersistemas.helpdesk.services.execptions.DataIntegrityViolationException;
+import com.inforcentersistemas.helpdesk.services.execptions.ObjectNotFoundException;
+
+import jakarta.validation.Valid;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository tecnicoRepository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = tecnicoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado no banco de dados ID: " + id));
+	}
+
+	public List<Cliente> findAll() {
+		return tecnicoRepository.findAll();
+	}
+
+	public Cliente Create(ClienteDTO dto) {
+		dto.setId(null);
+		validaPorCpfEEmail(dto);
+		Cliente tecnico = new Cliente(dto);
+		return tecnicoRepository.save(tecnico);
+	}
+
+	private void validaPorCpfEEmail(ClienteDTO dto) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(dto.getCpf());
+		if (obj.isPresent() && obj.get().getId() != dto.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+		}
+		
+		obj = pessoaRepository.findByEmail(dto.getEmail());
+		
+		if (obj.isPresent() && obj.get().getId() != dto.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
+		}
+	}
+
+	public Cliente update(Integer id, @Valid ClienteDTO dto) {
+		dto.setId(id);
+		Cliente oldObj = findById(id);
+		validaPorCpfEEmail(dto);
+		oldObj = new Cliente(dto);
+		return tecnicoRepository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser excluído!");
+		}
+		tecnicoRepository.deleteById(id);
+	}
+}
