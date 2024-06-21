8440b0be918be9252c9527dceced8bc6c410bcc3
José Harão P. Lima <harao.lima@yahoo.com.br>
2024-06-20 21:35:33 -0300

    criando componente header

--- FrontEnd/app-helpdesk/src/app/components/nav/nav/nav.component.ts
+++ FrontEnd/app-helpdesk/src/app/components/nav/nav/nav.component.ts
@@ -5,11 +5,12 @@ import { MatIcon } from '@angular/material/icon';
 import { MatListItem, MatNavList } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../header/header.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, MatNavList, MatListItem, MatIcon, MatLabel, RouterOutlet],
+  imports: [MatSidenavModule, MatButtonModule, MatNavList, MatListItem, MatIcon, MatLabel, RouterOutlet, HeaderComponent],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
