19c861f58f2534156e950a81c09ffdc98a7e5b17
José Harão P. Lima <harao.lima@yahoo.com.br>
2024-06-22 00:03:56 -0300

    logout efetuado

--- FrontEnd/app-helpdesk/src/app/components/nav/nav/nav.component.ts
+++ FrontEnd/app-helpdesk/src/app/components/nav/nav/nav.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../../services/auth.service';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatLabel } from '@angular/material/form-field';
@@ -6,6 +7,7 @@ import { MatListItem, MatNavList } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../header/header.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -15,10 +17,16 @@ import { HeaderComponent } from '../../header/header.component';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-    constructor(private router:Router){
+    constructor(private router:Router, private auth: AuthService, private toast: ToastrService){
 
     }
     ngOnInit(): void{
-        //this.router.navigate(["login"])
+
+    }
+
+    logout(){
+      this.router.navigate(["login"]);
+      this.auth.logout();
+      this.toast.info("Logout efetuado com sucesso!!!", "Logout", { timeOut:7000 });
     }
 }
